cf650ccac7bc9da9132541cddaf963b1758fcbcb
Mohammad Rahman <mbrahman@gmail.com>
2021-11-08 10:13:00 -0500

    added edit functionality for tasks and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,95 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$('.list-group').on('click', 'p', function() {
+  var text = $(this)
+    .text()
+    .trim()
+  
+  var textInput = $('<textarea>')
+    .addClass('form-control')
+    .val(text)
 
+  $(this).replaceWith(textInput)
 
+  textInput.trigger('focus')
+})
+
+// due date was clicked
+$('.list-group').on('click', 'span', function() {
+
+  // get current text
+  var date = $(this)
+    .text()
+    .trim()
+  console.log(date)
+
+  // create new input element
+  var inputDate = $('<input>')
+    .attr('type', 'text')
+    .addClass('form-control')
+    .val(date)
+
+  $(this).replaceWith(inputDate)
+
+  inputDate.trigger('focus')
+})
+
+$('.list-group').on('blur', 'textarea', function() {
+
+  // console.log(this)
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim()
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest('.list-group')
+    .attr('id')
+    .replace('list-', '')
+  
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest('.list-group-item')
+    .index()
+  
+  tasks[status][index].text = text
+  saveTasks();
+
+  // recreate p element
+  var taskP = $('<p>')
+    .addClass('m-1')
+    .text(text)
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP)
+})
+
+// save the information when clicked away
+$('.list-group').on('blur', 'input', function() {
+  var date = $(this)
+    .val()
+    .trim()
+  
+  var index = $(this)
+    .closest('.list-group-item')
+    .index()
+  
+  var status = $(this)
+    .closest('.list-group')
+    .attr('id')
+    .replace('list-', '')
+
+  tasks[status][index].date = date
+  saveTasks()
+
+  var dateSpan = $('<span>')
+    .addClass('badge badge-primary badge-pill')
+    .text(date)
+
+  $(this).replaceWith(dateSpan)
+})
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -93,5 +180,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
